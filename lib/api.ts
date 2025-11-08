813c4678213152179f3e3677438503abd8cf5552
Peter Le Souchu <peter22510@gmail.com>
2025-11-08 01:46:18 +0100

    fix: change api

--- lib/api.ts
+++ lib/api.ts
@@ -11,7 +11,7 @@ interface RefreshTokenResponse {
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001",
-  withCredentials: false, // Pas de cookies par défaut
+  withCredentials: true, // Pas de cookies par défaut
   headers: {
     "Content-Type": "application/json",
   },
